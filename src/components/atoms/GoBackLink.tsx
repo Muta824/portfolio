974f67ac725cdc928d5ce98d9bdb59991179d7ca
MUTA <muta.webdev@gmail.com>
2025-09-27 22:40:10 +0900

    fixed GoBackLink component

--- src/components/atoms/GoBackLink.tsx
+++ src/components/atoms/GoBackLink.tsx
@@ -1,9 +1,9 @@
 import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
-export const GoBackLink = ({ href }: { href?: string }) => {
+export const GoBackLink = ({ href = "" }: { href?: string }) => {
     return (
-        <Link href={`/${href}` || "/"} className="flex items-center gap-1 px-2 py-1 w-fit text-blue-600 rounded-md mb-4 text-md">
+        <Link href={`/${href}`} className="flex items-center gap-1 px-2 py-1 w-fit text-blue-600 rounded-md mb-4 text-md">
             <ArrowLeftIcon className="w-4 h-4" /> back to {href || "home"}
         </Link>
     )
