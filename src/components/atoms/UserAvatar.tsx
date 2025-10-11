739a256cb2095820be1de13c28f7154ed93b3695
MUTA <muta.webdev@gmail.com>
2025-10-11 12:00:11 +0900

    added dark mode in user avatar

--- src/components/atoms/UserAvatar.tsx
+++ src/components/atoms/UserAvatar.tsx
@@ -2,13 +2,17 @@ import { auth } from '@/auth'
 import Image from "next/image"
  
 export default async function UserAvatar() {
-  const session = await auth()
- 
-  if (!session?.user?.image) return null
- 
-  return (
-    <div>
-      <Image src={session.user.image} alt="User Avatar" width={32} height={32} className="rounded-full" />
-    </div>
-  )
+    const session = await auth()
+  
+    if (!session?.user?.image) return null
+  
+    return (
+        <Image 
+            src={session.user.image} 
+            alt="User Avatar" 
+            width={40} 
+            height={40} 
+            className="rounded-full border dark:border-white" 
+        />
+    )
 }
