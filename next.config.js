11291a8f09141b8b08ad7a788943b97a474edc02
MUTA <muta.webdev@gmail.com>
2025-12-02 13:01:47 +0900

    Add GitHub contributions image and update next.config.js for new hostname

--- next.config.js
+++ next.config.js
@@ -14,7 +14,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.pumbas.net',
+      },
     ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Navigation } from '@/components/organisms/Navigation';
 import { SkillsSection } from '@/components/organisms/SkillsSection';
 import { AppSection } from '@/components/organisms/appSection';
+import Image from 'next/image';
 import './globals.css';
 
 export default function Page() {
@@ -11,6 +12,15 @@ export default function Page() {
             
             {/* 技術スキルセクション */}
             <SkillsSection />
+
+            <Image 
+                className="mt-20 mx-auto"
+                src="https://github.pumbas.net/api/contributions/Muta824?colour=7ACE65" 
+                alt="Muta824's GitHub contributions" 
+                width={800}
+                height={800}
+                unoptimized
+            />
 
             {/* アプリケーションセクション */}
             <AppSection />
