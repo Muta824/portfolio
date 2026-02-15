5875e3467981982c1a129b34f3395a8b960a4418
Muta824 <yuuta20050927@gmail.com>
2026-02-16 08:41:12 +0900

    refactor: update ESLint configuration and linting script
    - Replaced FlatCompat with coreWebVitals from eslint-config-next for a more streamlined ESLint setup.
    - Updated the lint script in package.json to use 'eslint src' instead of 'next lint' for better control over linting process.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...coreWebVitals,
   {
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
@@ -30,5 +21,3 @@ const eslintConfig = [
     ]
   }
 ];
-
-export default eslintConfig;
